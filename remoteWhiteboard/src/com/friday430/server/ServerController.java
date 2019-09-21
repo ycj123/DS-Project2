2f454752d7dea5bebd7a047642b3410d13c4006c
jiaxinqin <jiqin@student.unimelb.edu.au>
2019-09-21 19:37:57 +1000

    serverController

--- remoteWhiteboard/src/com/friday430/server/ServerController.java
+++ remoteWhiteboard/src/com/friday430/server/ServerController.java
@@ -1,40 +1,59 @@
 package com.friday430.server;
 
 import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
-public class ServerController extends Thread{
-
+public class ServerController extends Thread {
 
 
     private ServerManagerService serverManagerService = new ServerManagerService();
     private ServerRMIService serverRMIService = new ServerRMIService();
 
 
-
-    public void handleClientRequest(String board_id){
-        String managerIP ;
-        String managerPort;
-
-    }
-
-    public String handleManagerReauest(String manager_keychain, String board_name){
-        ServerController serverController = new ServerController();
-        if (manager_keychain != null){
-            return this.generateBoardID(board_name);
+    public ArrayList handleClientRequest(String board_name) {
+        String board_id = this.generateBoardID(board_name);
+        if(serverManagerService.isBoard_id(board_id)) {
+            return serverManagerService.getManager(board_id);
         }
-        return ("Fail!");
+        else {
+            return null;
+        }
     }
 
-    private String generateBoardID( String board_name){
+    public String handleManagerReauest(String manager_keychain, String board_name, Socket clientManager) {
+        try {
+            String board_id = this.generateBoardID(board_name);
+            if (serverManagerService.isBoard_id(board_id))
+                return ("The board has been created!");
+            else {
+                String[] manager = this.getManagerIPPort(clientManager).split("#+");
+                serverManagerService.addManager(board_id, manager[0], manager[1]);
+                serverRMIService.createNewBoard(board_id, manager_keychain);
+                return "Create a board successfully!";
+            }
+        } catch (Exception e) {
+            return ("Create a board failed!!");
+        }
+    }
+
+    private String getManagerIPPort(Socket client) {
+        InetAddress addr = client.getInetAddress();
+        int port_client = client.getPort();
+        return addr + "#+" + port_client;
+    }
+
+    private String generateBoardID(String board_name) {
         byte[] secreteByte = null;
         try {
             secreteByte = MessageDigest.getInstance("md5").digest(board_name.getBytes());
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-        if(secreteByte != null) {
+        if (secreteByte != null) {
             String board_id = new BigInteger(1, secreteByte).toString(16);
             for (int i = 0; i < 32 - board_id.length(); i++) {
                 board_id = "0" + board_id;
@@ -43,7 +62,6 @@ public class ServerController extends Thread{
         }
         return "Fail";
     }
-
 
 
 //    public static void main(String[] args) {
--- remoteWhiteboard/src/com/friday430/server/ServerManagerService.java
+++ remoteWhiteboard/src/com/friday430/server/ServerManagerService.java
@@ -10,18 +10,29 @@ public class ServerManagerService {
 
     private ServerManagerDao serverManagerDao = ServerManagerDao.getInstance();
 
-//    public void loadServerManangerDict(){
-//        try {
-//            serverManageDict = (HashMap<String, ArrayList<String>>)serverManagerDao.readServerManager();
-//        }catch(Exception e){
-//            serverManageDict = new HashMap<String, ArrayList<String>>();
+    public void loadServerManangerDict(){
+        try {
+            serverManageDict = (HashMap<String, ArrayList<String>>)serverManagerDao.readServerManager();
+        }catch(Exception e){
+            serverManageDict = new HashMap<String, ArrayList<String>>();
+        }
+    }
+
+    public String saveServerManagerDict(){
+        serverManagerDao.writeServerManager();
+        return ("The board has exited!");
+    }
+//
+//    public boolean iskeyChain(String board_id, String key_chain){
+//        if(key_chain == idkeychain.get(board_id)){
+//
+//
 //        }
 //    }
 
-//    public String saveServerManagerDict(){
-//        serverManagerDao.writeServerManager();
-//        return ("The board has exited!");
-//    }
+    public boolean isBoard_id(String board_id){
+        return serverManageDict.containsKey(board_id);
+    }
 
     public boolean addManager(String board_id, String manager_ip, String manager_port){
         try{
