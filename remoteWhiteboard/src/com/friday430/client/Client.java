dc1353c7f99e7732a5b14f0537fdb4ed62769896
jiaxinqin <jiqin@student.unimelb.edu.au>
2019-10-18 18:51:17 +1100

    client

--- remoteWhiteboard/src/com/friday430/client/Client.java
+++ remoteWhiteboard/src/com/friday430/client/Client.java
@@ -4,16 +4,36 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Client {
+
+//    public static void main(String[] args) throws IOException, InterruptedException {
+//
+//        System.out.println("Join as a manager/client:");
+//        Scanner sc = new Scanner(System.in);
+//        String insert = sc.next();
+//        System.out.println(insert);
+//        //用户面板输入来执行条件体
+//        if (insert.equals("client") ){
+//            ClientController cc = new ClientController();
+//        }else if (insert.equals("manager")){
+//            ManagerController mc = new ManagerController();
+//        }else{
+//            System.out.println("please choose between  'manager' and 'client'");
+//        }
+
+
     public static void main(String[] args){
 
         if (args.length == 0){
-            ManagerController mc = new ManagerController("default_board", "127.0.0.1", "4444");
+            ManagerController mc1 = new ManagerController("default_board", "127.0.0.1", 4444);
+            ManagerController mc2 = new ManagerController("default_board", "127.0.0.1", 3758);
+
         }
         if (args.length == 2) {
             if (args[0].equals("client")) {
                 ClientController cc = new ClientController(args[1], "127.0.0.1", "5555");
             } else if (args[0].equals("manager")) {
-                ManagerController mc = new ManagerController(args[1], "127.0.0.1", "4444");
+                ManagerController mc1 = new ManagerController(args[1], "127.0.0.1", 4444);
+                ManagerController mc2 = new ManagerController(args[1], "127.0.0.1", 3758);
             } else {
                 System.exit(1);
             }
@@ -22,10 +42,12 @@ public class Client {
             if (args[0].equals("client")) {
                 ClientController cc = new ClientController(args[1], args[2], args[3]);
             } else if (args[0].equals("manager")) {
-                ManagerController mc = new ManagerController(args[1], args[2], args[3]);
+                ManagerController mc1 = new ManagerController(args[1], args[2], 4444);
+                ManagerController mc2 = new ManagerController(args[1], args[2], 3758);
             } else {
                 System.exit(1);
             }
         }
     }
+
 }
